2688bff98906ef30f2f1885a7c543e6bb8dfb842
Eng_MIR <138880782+Eng-MIR@users.noreply.github.com>
2026-05-22 05:47:25 +0300

    Update streamlit_application.py

--- App/streamlit_application.py
+++ App/streamlit_application.py
@@ -114,42 +114,4 @@ if st.button("Predict Churn"):
 
     # Display input data
     st.subheader("Customer Input Data")
-    st.dataframe(input_data)import streamlit as st
-import pandas as pd
-import joblib
-import numpy as np
-
-model = joblib.load('Models/customer_churn_model.pkl')
-scaler = joblib.load('Models/scaler.pkl')
-
-st.title('Customer Churn Prediction Dashboard')
-
-st.sidebar.header('Customer Information')
-
-monthly_charges = st.sidebar.slider('Monthly Charges', 0, 200, 70)
-tenure = st.sidebar.slider('Tenure', 0, 72, 24)
-total_charges = st.sidebar.slider('Total Charges', 0, 10000, 2000)
-
-input_data = pd.DataFrame({
-    'MonthlyCharges': [monthly_charges],
-    'tenure': [tenure],
-    'TotalCharges': [total_charges]
-})
-
-# Reshape to 2D: (1 sample, n_features)
-#input_array = np.array(input_data).reshape(1, -1) 
-
-if st.button('Predict Churn'):
-    
-    #scaled_data = scaler.transform(input_array)
-    scaled_data = scaler.transform(input_data.to_numpy())
-    
-    prediction = model.predict(scaled_data)[0]
-    probability = model.predict_proba(scaled_data)[0][1]
-
-    st.subheader('Prediction Result')
-
-    if prediction == 1:
-        st.error(f'Customer is likely to churn. Probability: {probability:.2f}')
-    else:
-        st.success(f'Customer is not likely to churn. Probability: {probability:.2f}')
+    st.dataframe(input_data)
