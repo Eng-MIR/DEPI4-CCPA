3078263e9d4ce15354b4c8283857bcb9df7d7e9f
Eng_MIR <138880782+Eng-MIR@users.noreply.github.com>
2026-05-22 09:37:12 +0300

    Create FastAPI_app.py

---
+++ App/FastAPI_app.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+
+app = FastAPI()
+
+model = joblib.load('customer_churn_model.pkl')
+scaler = joblib.load('scaler.pkl')
+
+class CustomerData(BaseModel):
+    gender: int
+    SeniorCitizen: int
+    Partner: int
+    Dependents: int
+    tenure: int
+    PhoneService: int
+    MultipleLines: int
+    InternetService: int
+    OnlineSecurity: int
+    OnlineBackup: int
+    DeviceProtection: int
+    TechSupport: int
+    StreamingTV: int
+    StreamingMovies: int
+    Contract: int
+    PaperlessBilling: int
+    PaymentMethod: int
+    MonthlyCharges: float
+    TotalCharges: float
+
+@app.get('/')
+def home():
+    return {'message': 'Customer Churn Prediction API'}
+
+@app.post('/predict')
+def predict(data: CustomerData):
+
+    input_data = pd.DataFrame([data.dict()])
+
+    scaled_data = scaler.transform(input_data)
+
+    prediction = model.predict(scaled_data)[0]
+    probability = model.predict_proba(scaled_data)[0][1]
+
+    return {
+        'prediction': int(prediction),
+        'churn_probability': float(probability)
+    }
